92bf944480a0adceb3ed8ad2eef4f24377ed907a
M <45332429+akmhel@users.noreply.github.com>
2019-09-25 09:48:48 +0100

    Update lets correct - work on this.py

--- lets correct - work on this.py
+++ lets correct - work on this.py
@@ -10,7 +10,7 @@ import random
 import sklearn
 from nltk.corpus import stopwords
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extrqction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import linear_kernel
 from scipy.sparse.linalg import svds
