f55cfa5ea906434eb99b17bd18cf3c722a01d52c
M <45332429+akmhel@users.noreply.github.com>
2019-09-25 09:44:35 +0100

    Update lets correct - work on this.py

--- lets correct - work on this.py
+++ lets correct - work on this.py
@@ -28,20 +28,20 @@ from scipy.sparse.linalg import svds
 
 
 
-users = pd.read_csv('lucid blog.csv/users.csv', encoding='latin-1')
+users = pd.read_csv('lucid blog.csv/users.csv')
 users.head()
 
-lucid_tfidf = TfidVectorizer(stop_words='English')
+lucid_tfidf = TfidVectorizer(stopwords='English')
 users['short_bio'] = users['short_bio'].fillna('')
 u_matx = lucid_tfidf.fit_transform(users['short_bio'])
 
 u_matx.shape
 
-cos_sim = linear_kernel(u_matx, u_matx)
+cosine_similarity = linear_kernel(u_matx, u_matx)
 
 indices = pd.Series(users['name'].index)
 
-def recommend(index, cosine_sim=cos_sim):
+def recommend(index, cosine_sim=cosine_similarity):
     id = indices[index]
     similarity_scores = list(enumerate(cosine_sim[id]))
     similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
@@ -56,24 +56,24 @@ recommend(2)
 recommend(6)
 
 
-posts = pd.read_csv('lucid blog.csv/posts.csv', encoding='latin-1')
+posts = pd.read_csv('lucid blog.csv/posts.csv')
 posts.head()
 
-lucids_tfidf = TfidVectorizer(stop_words='English')
+lucids_tfidf = TfidVectorizer(stopwords='English')
 posts['title'] = posts['title'].fillna('')
 p_matx = lucids_tfidf.fit_transform(posts['title'])
 
 p_matx.shape
 
-cosz_sim = linear_kernel(p_matx, p_matx)
+cosine_similarity = linear_kernel(p_matx, p_matx)
 
 indicesz = pd.Series(posts['content'].index)
 
-def recommend(index, cosine_sim=cosz_sim):
+def recommend(index, cosine_sim=cosine_similarity):
     id = indicesz[index]
     similarity_scores = list(enumerate(cosine_sim[id]))
     similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
-    similarity_scores = similarity_scores[1:4]
+    similarity_scores = similarity_scores[1:6]
 
     lud_index = [i[0] for i in similarity_scores]
 
@@ -84,24 +84,24 @@ recommend(6)
 recommend(10)
 
 
-notifications = pd.read_csv('lucid blog.csv/notifications.csv', encoding='latin-1')
+notifications = pd.read_csv('lucid blog.csv/notifications.csv')
 notifications.head()
 
-lucidss_tfidf = TfidVectorizer(stop_words='English')
+lucidss_tfidf = TfidVectorizer(stopwords='English')
 notifications['comment'] = notifications['comment'].fillna('')
 n_matx = lucidss_tfidf.fit_transform(notifications['comment'])
 
 n_matx.shape
 
-coszs_sim = linear_kernel(n_matx, n_matx)
+cosine_similarity = linear_kernel(n_matx, n_matx)
 
 indiceszs = pd.Series(notifications['post_id'].index)
 
-def recommend(index, cosine_sim=coszs_sim):
+def recommend(index, cosine_sim=cosine_similarity):
     id = indiceszs[index]
     similarity_scores = list(enumerate(cosine_sim[id]))
     similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
-    similarity_scores = similarity_scores[1:4]
+    similarity_scores = similarity_scores[1:6]
 
     lud_index = [i[0] for i in similarity_scores]
 
